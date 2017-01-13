eb16e6ff05bb4bac36eef458f6ac506596aa7b46
Maciej Długoszek <muloni@gmail.com>
2017-01-13 23:16:43 +0100

    Smaller letters

--- config.go
+++ config.go
@@ -1,5 +1,5 @@
 package main
 
 const (
-	pixelStep = 8 // Defines a side of squares inside the GIFs (also this must be equal to the size of the letters images)
+	pixelStep = 7 // Defines a side of squares inside the GIFs (also this must be equal to the size of the letters images)
 )
